7fad6e8ffe1acfd2f2693248574b8ade9ea432df
Rafael <111534933+RafaKHR@users.noreply.github.com>
2024-08-23 16:11:26 -0300

    Add files via upload

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,24 @@
+let num1 = 10;
+let num2 = 5;
+let resultadoSoma;
+let num3 = 8;
+let num4 = 7;
+let resultadoSub;
+let num5 = 9;
+let num6 = 3;
+let resultadoMult;
+let num7 = 4;
+let num8 = 1;
+let resultadoDiv;
+
+resultadoSoma = num1 + num2;
+console.log("Resultado da Soma entre "  + num1 + " e " + num2 + " é: " + resultadoSoma);
+
+resultadoSub = num3 - num4;
+console.log("Resultado da Subtração entre "  + num3 + " e " + num4 + " é: " + resultadoSub);
+
+resultadoMult = num5 * num6;
+console.log("Resultado da Multiplicação entre "  + num5 + " e " + num6 + " é: " + resultadoMult);
+
+resultadoDiv = num7 / num8;
+console.log("Resultado da Divisão entre "  + num7 + " e " + num8 + " é: " + resultadoDiv);
