871ebfbe6c690b0dfcf8a912038c987816583f46
Rafael <111534933+RafaKHR@users.noreply.github.com>
2024-09-17 15:16:24 -0300

    Delete outros directory

--- outros/modulo/main.js
+++
@@ -1,7 +0,0 @@
-const calc = require('./calculadora');
-
-let a = 10;
-let b = 5;
-
-console.log(`A soma entre ${a} e ${b} é ${calc.somar(a, b)}`);
-console.log(`A multiplicação entre ${a} e ${b} é ${calc.multiplicar(a, b)}`);
