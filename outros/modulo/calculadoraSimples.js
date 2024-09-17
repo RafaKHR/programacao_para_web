e88d83337cb035978229366d8ef6273ed4832916
Rafael <111534933+RafaKHR@users.noreply.github.com>
2024-09-17 15:32:59 -0300

    Add files via upload

---
+++ outros/modulo/calculadoraSimples.js
@@ -0,0 +1,14 @@
+function somar(a, b){
+    return a + b;
+}
+
+function multiplicar(a, b){
+    return a * b;
+}
+
+const PI = 3.14;
+
+module.exports = {
+    somar,
+    multiplicar
+}
---
+++ outros/modulo/main.js
@@ -0,0 +1,7 @@
+const calc = require('./calculadora');
+
+let a = 10;
+let b = 5;
+
+console.log(`A soma entre ${a} e ${b} é ${calc.somar(a, b)}`);
+console.log(`A multiplicação entre ${a} e ${b} é ${calc.multiplicar(a, b)}`);
