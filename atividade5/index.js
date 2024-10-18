91f5373bb3b531ee31c70747c99e971c78985e07
Rafael <111534933+RafaKHR@users.noreply.github.com>
2024-10-18 09:32:59 -0300

    Add files via upload

---
+++ atividade5/index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const app = express();
+const mustacheExpress = require('mustache-express');
+
+app.engine('html', mustacheExpress());
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+app.use(express.urlencoded({extended: true}));
+
+app.get('/', (req, res)=>{
+    let erro_form = req.query.erro_form;
+    let nome = req.query.nome;
+    let data_agendamento = req.query.data_agendamento;
+    let endereco = req.query.endereco;
+    let telefone = req.query.telefone;
+    res.render('index.html', {erro_form, nome, data_agendamento, endereco, telefone});
+});
+
+app.post('/dados', (req, res)=>{
+    let dados = {
+        nome: req.body.nome,
+        data_agendamento: req.body.data_agendamento,
+        endereco: req.body.endereco,
+        telefone: req.body.telefone
+    }
+    let erro_form = false;
+    if(dados.nome.length == 0){
+        erro_form = true;
+    }
+    if(dados.data_agendamento.length == 0){
+        erro_form = true;
+    }
+    if(dados.endereco.length == 0){
+        erro_form = true;
+    }
+    if(dados.telefone.length == 0){
+        erro_form = true;
+    }
+
+    if(erro_form){
+        res.redirect(`/?erro_form=1&nome=${dados.nome}&data_nascimento=${dados.data_agendamento}&endereco=${dados.endereco}&telefone=${dados.telefone}`);
+    }else {
+        res.render('dados.html', {dados});
+    }
+});
+
+const PORT = 8080;
+app.listen(PORT, ()=> {
+    console.log('Estamos dentro!!' + PORT);
+});
