2992542d34e3137ad4e3506ae0d79851db1f05a4
Rafael <111534933+RafaKHR@users.noreply.github.com>
2024-11-07 17:24:05 -0300

    Delete routers directory

--- routers/usuarioRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const usuarioController = require('../controllers/usuarioController');
-
-router.get('/cadastro', usuarioController.cadastroView);
-router.get('/login', usuarioController.loginView);
-router.post('/cadastro', usuarioController.postCadastro);
-
-module.exports = router;
