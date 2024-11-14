5099bb798495aab88942038da579b09661cfff9e
Rafael <111534933+RafaKHR@users.noreply.github.com>
2024-11-14 10:33:08 -0300

    Delete PaiDePet directory

--- PaiDePet/routers/usuarioRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const usuarioController = require('../controllers/usuarioController');
-
-router.get('/login-cadastro', usuarioController.loginCadastroView);
-router.post('/cadastro', usuarioController.postCadastro);
-
-module.exports = router;
